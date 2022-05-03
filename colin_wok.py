d40b3f73654ac69d15b0f404c8ec3e0edb5c1223
p22cobrien <p22cobrien@fairfieldprep.org>
2022-05-03 10:14:42 -0400

    WIP 5/3/2022

--- colin_wok.py
+++ colin_wok.py
@@ -1,21 +1,9 @@
 # Author: CMOB 5/2/2022
 
-import os 
 import folium
-from folium import plugins
-import rioxarray as rxr
-import earthpy as et
-import earthpy.spatial as es
+import pandas as pd
+import geopandas
 
+m = folium.Map(location=[74.0060, 40.7128])
 
-# Import data from EarthPy
-data = et.data.get_data('colorado-flood')
-
-# Set working directory to earth-analytics
-os.chdir(os.path.join(et.io.HOME, 'earth-analytics', 'data'))
-
-# Create a map using the Map() function and the coordinates for Boulder, CO
-m = folium.Map(location=[40.0150, -105.2705])
-
-# Display the map
-m
+m.save('map.html')
