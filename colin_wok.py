858ea67078e4dceb6dcf62f247ab8367f824ad8e
p22cobrien <p22cobrien@fairfieldprep.org>
2022-05-25 10:35:13 -0400

    5/25/2022

--- colin_wok.py
+++ colin_wok.py
@@ -1,36 +1,46 @@
 # Author: CMOB 5/2/2022
 
 # all of the modules
-# import os
 import folium
-# from folium import IFrame, Popup, plugins
 import pandas as pd
-# import geopandas
-# import rioxarray as rxr
-# import earthpy as et
-# import earthpy.spatial as es
-# import json
+from geopy.geocoders import Nominatim
+
+lon = []
+lat = []
+title = []
+loc = []
+
+dataset = "/Users/p22cobrien/Desktop/Project/final-project-ian-colin-1/Food Bank For NYC Open Sites copy.csv"
+csv = pd.read_csv(dataset)
+df = pd.DataFrame(csv)
+addresses = df.loc[:, "address"]
+names = df.loc[:, "name"]
+for name in names:
+    title.append(name)
+
+for address in addresses:
+    locator = Nominatim(user_agent="my-geocoder")
+    location = locator.geocode("{0}".format(address), timeout=100)
+    lon.append(location.longitude)
+    lat.append(location.latitude)
+    loc.append(address)
+
 
 # setting up the map
 m = folium.Map(location=[40.7128, -74.0060])
-
-lon = [-73.8589411, -73.8494246, -73.851252]
-lat = [40.889903, 40.8910298, 40.8868126]
-name = ['MOUNT PISGAH BAPTIST CHURCH', 'THE GRACE OF GOD MINISTRIES – CHURCH INC', 'Bronx Bethany Community Corporation Food Provider']
-loc = ['709 EAST 228 STREET, BRONX, NY, 10466', '963 EAST 233 STREET, BRONX, NY, 10466', '971 EAST 227 STREET, BRONX, NY, 10466']
 
 # data frame to put into markers on map
 data = pd.DataFrame({
     'lon': lon,
     'lat': lat,
-    'name': name,
+    'title': title,
     'loc': loc
 }, dtype=str)
 
 # takes the data frame and puts into markers the on the map.
 for i in range(0, len(data)):
     html = f"""
-          <h1> {data.iloc [i]['name']} </h1>
+          <h1> {data.iloc [i]['title']} </h1>
           <p> Address: {data.iloc[i]['loc']} </p>
             """
     iframe = folium.IFrame(html=html, width=200, height=200)
